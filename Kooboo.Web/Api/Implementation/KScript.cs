1ee314c9dd5b9136f8f8238681036da57fc63aca
李凤焕 <huanent@icloud.com>
2020-02-19 14:09:31 +0800

    getlib api add cache

--- Kooboo.Web/Api/Implementation/KScript.cs
+++ Kooboo.Web/Api/Implementation/KScript.cs
@@ -8,18 +8,42 @@ using Kooboo.Sites.Scripting;
 using Kooboo.Web.Frontend.KScriptDefine;
 using Kooboo.Web.ViewModel;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Kooboo.Web.Api.Implementation
 {
     public class KScriptApi : IApi
     {
+        class Cache
+        {
+            public DateTime Expired { get; set; }
+            public string Value { get; set; }
+        }
 
         private readonly Lazy<string> _defineContent;
+
+
+        private static readonly ConcurrentDictionary<string, Cache> _cdnLibCache = new ConcurrentDictionary<string, Cache>();
+
+        static KScriptApi()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(new TimeSpan(3, 0, 0, 0));
+                var expiredKeys = _cdnLibCache.Where(w => w.Value.Expired < DateTime.Now).Select(s => s.Key).ToArray();
+
+                foreach (var key in expiredKeys)
+                {
+                    _cdnLibCache.TryRemove(key, out _);
+                }
+            });
+        }
 
         public KScriptApi()
         {
@@ -50,9 +74,21 @@ namespace Kooboo.Web.Api.Implementation
         public string GetLib(string name, ApiCall call)
         {
             var script = call.WebSite.SiteDb().Scripts.GetByNameOrId($"{name}.js");
+            if (script == null) call.WebSite.SiteDb().Scripts.GetByNameOrId($"{name}.min.js");
             if (script != null) return script.Body;
-            var client = new System.Net.WebClient();
-            return client.DownloadString($"https://cdn.jsdelivr.net/npm/{name}");
+
+            if (_cdnLibCache.TryGetValue(name, out var cache))
+            {
+                cache.Expired = DateTime.Now.AddMonths(1);
+                return cache.Value;
+            }
+            else
+            {
+                var client = new System.Net.WebClient();
+                var result = client.DownloadString($"https://cdn.jsdelivr.net/npm/{name}");
+                _cdnLibCache.TryAdd(name, new Cache { Expired = DateTime.Now.AddMonths(1), Value = result });
+                return result;
+            }
         }
     }
 }
