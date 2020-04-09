a867bddb30da3f53c86d15a514a03c4eca873996
Linweimin-kooboo <weimin-kooboo@outlook.com>
2020-04-09 09:44:00 +0800

    add zto

---
+++ Kooboo.Sites/Logistics/Methods/zto/Models/ZopRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+
+namespace Kooboo.Sites.Logistics.Methods.zto.Models
+{
+    public class ZopRequest
+    {
+        public string url { set; get; }
+        public NameValueCollection requestParams { get; }
+        public int timeout = 2000;
+
+        public ZopRequest()
+        {
+            this.requestParams = new NameValueCollection();
+        }
+
+        public ZopRequest addParam(string k, string v)
+        {
+            requestParams.Add(k, v);
+            return this;
+        }
+
+    }
+}
---
+++ Kooboo.Sites/Logistics/Methods/zto/ZTOSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kooboo.Sites.Logistics.Methods.uda
+{
+    public class ZTOSetting : ILogisticsSetting
+    {
+        private const string name = "ZTO";
+
+        public string Name => name;
+
+        public string AppKey { get; set; }
+
+        public string AppSecret { get; set; }
+
+        public string backurl { get; set; }
+    }
+}
