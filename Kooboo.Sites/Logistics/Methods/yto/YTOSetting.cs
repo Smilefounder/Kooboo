8d3dc161cf152a66acb45569a49024d580b946fa
Linweimin-kooboo <weimin-kooboo@outlook.com>
2020-04-10 18:11:43 +0800

    add yto

---
+++ Kooboo.Sites/Logistics/Methods/yto/YTOSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kooboo.Sites.Logistics.Methods.yto
+{
+    public class YTOSetting : ILogisticsSetting
+    {
+        private const string name = "YTO";
+
+        public bool UseSandBox { get; set; }
+
+        public string Name => name;
+
+        public string AppKey { get; set; }
+
+        public string Version { get; set; }
+
+        public string UserId { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string ServerURL => UseSandBox ? "http://58.40.16.122:8080" : "http://japi.zto.cn";
+    }
+}
---
+++ Kooboo.Sites/Logistics/Methods/yto/lib/YTOClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Kooboo.Sites.Logistics.Methods.yto.lib
+{
+    public class YTOClient
+    {
+        private const string TransportPrice = "yto.Marketing.TransportPrice";
+        private readonly YTOSetting setting;
+
+        public YTOClient(YTOSetting setting)
+        {
+            this.setting = setting;
+        }
+
+        public string Post(string url, string body, string method)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("user_id", setting.UserId);
+            parameters.Add("app_key", setting.AppKey);
+            parameters.Add("format", "JSON");
+            parameters.Add("method", method);
+            parameters.Add("v", DateTime.UtcNow.ToString());
+            parameters.Add("timestamp", setting.Version);
+            var sign = MakeSign(parameters, method);
+        }
+
+        private string MakeSign(Dictionary<string, string> paras, string method)
+        {
+            var parameters = new SortedDictionary<string, string>(paras);
+
+            var signString = parameters.Select(it => string.Join("", it.Key, it.Value)).ToString();
+
+            var md5 = MD5.Create();
+            var bs = md5.ComputeHash(Encoding.UTF8.GetBytes(setting.SecretKey + signString));
+            return System.Text.Encoding.UTF8.GetString(bs).ToUpper();
+        }
+    }
+}
