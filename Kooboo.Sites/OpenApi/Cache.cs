572d675326f9cdfcdea852d06e3e3bdd2918db61
huanent <huanent@icloud.com>
2021-05-28 10:09:30 +0800

    Add openapi Cache cleanTask

--- Kooboo.Sites/OpenApi/Cache.cs
+++ Kooboo.Sites/OpenApi/Cache.cs
@@ -7,6 +7,9 @@ using Kooboo.Sites.Scripting.KDefine;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Kooboo.Sites.OpenApi
 {
@@ -15,27 +18,16 @@ namespace Kooboo.Sites.OpenApi
         static readonly ConcurrentDictionary<Guid, Cache> _cache = new ConcurrentDictionary<Guid, Cache>();
         readonly Lazy<Dictionary<string, Document>> _documents;
         readonly Lazy<string> _defineString;
+        readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
         public Dictionary<string, Document> Documents => _documents.Value;
         public string DefineString => _defineString.Value;
 
         public Cache(WebSite webSite)
         {
-            _documents = new Lazy<Dictionary<string, Document>>(() =>
-            {
-                var result = new Dictionary<string, Document>();
-                var list = webSite.SiteDb().OpenApi.All();
-
-                foreach (var item in list)
-                {
-                    var name = Helpers.StandardName(item.Name);
-                    if (!result.ContainsKey(name)) result.Add(name, new Document(item));
-                }
-
-                return result;
-            });
-
+            _documents = new Lazy<Dictionary<string, Document>>(() => GetDocuments(webSite), true);
             _defineString = new Lazy<string>(GetDefinesString, true);
+            StartCleanTask();
         }
 
         public static Cache Get(WebSite webSite)
@@ -45,7 +37,27 @@ namespace Kooboo.Sites.OpenApi
 
         public static void Remove(WebSite webSite)
         {
-            _cache.TryRemove(webSite.Id, out _);
+            _cache.TryRemove(webSite.Id, out var cache);
+            cache.StopCleanTask();
+        }
+
+        public void StopCleanTask()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        Dictionary<string, Document> GetDocuments(WebSite webSite)
+        {
+            var result = new Dictionary<string, Document>();
+            var list = webSite.SiteDb().OpenApi.All();
+
+            foreach (var item in list)
+            {
+                var name = Helpers.StandardName(item.Name);
+                if (!result.ContainsKey(name)) result.Add(name, new Document(item));
+            }
+
+            return result;
         }
 
         string GetDefinesString()
@@ -88,8 +100,43 @@ namespace Kooboo.Sites.OpenApi
                 }
             });
 
-            var result= new DefineStringify(defines.ToArray()).ToString();
+            var result = new DefineStringify(defines.ToArray()).ToString();
             return result;
+        }
+
+        void StartCleanTask()
+        {
+            var cleanTask = new Task(async () =>
+            {
+                while (!_cancellationTokenSource.IsCancellationRequested)
+                {
+                    try
+                    {
+                        await Task.Delay(TimeSpan.FromDays(1), _cancellationTokenSource.Token);
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                    if (_documents.IsValueCreated)
+                    {
+                        var operations = _documents.Value.SelectMany(doc => doc.Value.Operations);
+
+                        foreach (var operation in operations)
+                        {
+                            var expireItems = operation.Value.Caches.Where(w => DateTime.UtcNow > w.Value.Item1).Select(s => s.Key);
+
+                            foreach (var item in expireItems)
+                            {
+                                operation.Value.Caches.TryRemove(item, out _);
+                            }
+                        }
+                    }
+                }
+
+            }, TaskCreationOptions.LongRunning);
+
+            cleanTask.Start();
         }
     }
 
